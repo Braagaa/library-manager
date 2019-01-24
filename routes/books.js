4ef0bc39e6e9f83ad32631dc64e639ba30252a00
Michael Braga <michael_braga@outlook.com>
2019-01-23 20:25:20 -0500

    get /, get /books, get books/new complete

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const {Book} = require('../models/');
+const R = require('ramda');
+
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hi Everybody!'));
+const render = R.invoker(2, 'render');
+
+router.get('/', (req, res) => {
+    Book.findAll()
+        .then(R.objOf('books'))
+        .then(R.mergeRight({title: 'Books'}))
+        .then(render('index', R.__, res));
+});
+
+router.get('/new', (req, res) => 
+    res.render('new-book', {title: 'New Book'})
+);
 
 module.exports = router;
